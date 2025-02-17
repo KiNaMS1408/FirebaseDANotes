9ca484b300c716a7194eec3e4a1a57c5daa6141f
Franz Neußer <140851101+KiNaMS1408@users.noreply.github.com>
2025-02-17 09:59:51 +0100

    save  onSnapshot and collectData

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Note } from "../interfaces/note.interface";
-import { Firestore, collection, doc, collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, doc, collectionData, onSnapshot} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,10 +12,60 @@ export class NoteListService {
   normalNotes: Note[] = [];
 
   items$;
+  items;
+
+  unsubList;
+  unsubSingle;
+
   firestore: Firestore = inject(Firestore);
   
   constructor() {
+
+    this.unsubList = onSnapshot(this.getNotesRef(),(list) => {
+      list.forEach(element => {
+        // dokumenten ID ausloggen lassen
+        console.log(element.id);
+        // Dokumenten daten
+        console.log(element.data());
+        // Dokumenten daten mit setNoteObject() Funktion
+        console.log(this.setNoteObject(element.data, element.id));
+      });
+    });
+ 
+    this.unsubSingle = onSnapshot(this.getSingleDocRef("notes", "3CkVi2C4d55KgUC1l75T"),(element) => {
+    });
+
+    // this.unsubSingle();
+    // this.unsubList()
+
     this.items$ = collectionData(this.getNotesRef());
+    this.items = this.items$.subscribe ((list) => {
+      list.forEach(element => {
+        console.log(element);
+      });
+    })
+  }
+
+  ngonDestroy(){
+    this.items.unsubscribe();
+  }
+
+  subTrashList(){
+
+  }
+
+  subNotesList(){
+
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    return{
+      id: id || "",
+      type: obj.type || "note",
+      title: obj.title || "",
+      content: obj.content || "",
+      marked: obj.marked || false,
+    }
   }
 
   getNotesRef(){
